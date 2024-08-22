a759e30f196dad00775db4da6c4090ba7e7720b8
alexiscruz1403 <jr.alexis.cruz.2003@gmail.com>
2024-08-22 12:51:22 -0300

    modificaciones ej4,ej5 y ej6

--- TP1/Ejercicio4/Vista/index.php
+++ TP1/Ejercicio4/Vista/index.php
@@ -3,27 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/estilosEj4.css">
+    <link rel="stylesheet" href="assets/css/estilosTp1Ej4.css">
     <script src="javascript/validarEj4.js"></script>
     <title>Home</title>
 </head>
 <body>
-    <form action="action/mostrar.php" method="get">
+    <form action="action/mostrar.php" method="post">
         <div class="campo">
-            <label for="nombre">Nombre:</label>
+            <label for="nombre">Nombre</label>
             <input type="text" name="nombre" id="nombre" required>
+            <label for="nombre" id="nombreError" class="labelError">Debe ingresar solo letras. No puede haber espacios consecutivos</label>
         </div>
         <div class="campo">
-            <label for="apellido">Apellido:</label>
+            <label for="apellido">Apellido</label>
             <input type="text" name="apellido" id="apellido" required>
+            <label for="nombre" id="apellidoError" class="labelError">Debe ingresar solo letras. No puede haber espacios consecutivos</label>
         </div>
         <div class="campo">
-            <label for="edad">Edad:</label>
+            <label for="edad">Edad</label>
             <input type="number" name="edad" id="edad" min="1" required>
         </div>
         <div class="campo">
-            <label for="direccion">Direccion:</label>
+            <label for="direccion">Direccion</label>
             <input type="text" name="direccion" id="direccion" required>
+            <label for="nombre" id="direccionError" class="labelError">Debe ingresar solo letras o numeros. No puede haber espacios o puntos consecutivos</label>
         </div>
         <input type="submit" value="Enviar">
     </form>
--- TP1/Ejercicio6/Vista/action/mostrar.php
+++ TP1/Ejercicio6/Vista/action/mostrar.php
@@ -23,7 +23,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/estilosEj6.css">
+    <link rel="stylesheet" href="../assets/css/estilosTp1Ej6.css">
     <title>Mostrar</title>
 </head>
 <body>
